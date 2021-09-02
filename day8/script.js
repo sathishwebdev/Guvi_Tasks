19f6e36551b0c8111caf2832ef4f39fb418e010d
Sathish Kumar <84026483+sathishwebdev@users.noreply.github.com>
2021-09-02 15:44:07 +0530

    Update script.js
    circle class updated #quest2 completed

--- day8/script.js
+++ day8/script.js
@@ -47,6 +47,60 @@ console.log(CRMovie.getDetail())
 
 
 
+// quest No. 2 
+// coverting uml diagram to class
+// create a class circle
+
+class Circle{
+    // constructor with arguments to get radius , color
+    constructor(radius, color){
+        this.radius = radius
+        this.color = color
+    }
+    // method to return a radius
+    getRadius = () => this.radius
+    
+    //method to set radius
+    setRadius(rad){
+        this.radius = rad;
+        console.log(`Radius of the circle is changed to ${rad}`)
+    }
+
+    //method to get Color
+    getColor = () => this.color
+
+    //method to  set color
+
+    setColor(col){
+        this.color = col
+        console.log(`Color of the circle is changed to ${col}`)
+    }
+
+    //method to get a info of circle as string
+    toString=()=>`circle[radius = ${this.radius}, color = ${this.color}]`
+
+    //calculate area of circle
+
+    getArea = () => Math.PI*(this.radius*this.radius)
+
+    // calculate circumference of the circle
+    
+    getCircumference =() => 2 * Math.PI * this.radius
+}
+
+const redCircle = new Circle(2.4, "red")
+
+// get the info of redCircle using methods of class Circle
+console.log(`redCircle's Info \n Radius : ${redCircle.getRadius()} \n Color : ${redCircle.getColor()} \n Area : ${redCircle.getArea()} \n Circumference : ${redCircle.getCircumference()} \n`);
+
+// set the color as blue
+redCircle.setColor("blue")
+
+//set thr radius as 3
+redCircle.setRadius(3.0)
+
+//getting the changed color and radius as string by toString method
+console.log(redCircle.toString())
 
 
 
