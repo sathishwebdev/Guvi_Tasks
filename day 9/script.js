a1e6c99fad97259151cb795e2c2d1d85d03d8669
Sathish Kumar <sathishweb27@gmail.com>
2021-09-24 15:22:06 +0530

    just another commit

--- day 9/script.js
+++ day 9/script.js
@@ -1,10 +1,10 @@
 var xml = new XMLHttpRequest();
 
-xml.open("GET", "https://restcountries.eu/rest/v2/all", true);
+xml.open("GET", "https://restcountries.com/v3/all", true);
 xml.send()
 xml.onload = function(){
 var data = JSON.parse(this.response)
-// console.log(data);
+console.log(data);
 //a) countries from Asia
 console.log(data.filter(countries=> countries.region === "Asia" ));
 
@@ -12,7 +12,7 @@ console.log(data.filter(countries=> countries.region === "Asia" ));
 console.log(data.filter(countries => countries.population < 200000 ))
 
 //c) get name, capital, flag using forEach
-data.forEach(countries=> console.log( `name : ${countries.name} | Capital : ${countries.capital} | flag : ${countries.flag}`));
+data.forEach(countries=> console.log( `name : ${countries.name.common} | Capital : ${countries.capital} | flag : ${countries.flags[1]}`));
 
 //d) calculate the populations by using reduce
 console.log(data.reduce((accum, popu)=> accum + popu.population, 0))
